909006fe27a75cc4f56ee8ed4a22054f6c23e4c7
yousef0090 <54067011+yousef0090@users.noreply.github.com>
2020-01-06 00:13:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ client.on('message', msg => {  //WESO#0001
   });
 
 client.on('message', msg => {  //WESO#0001
-    if (msg.content === 'ويسو') {  //WESO#0001
-      msg.reply('**ويسو مات**');  //WESO#0001
+    if (msg.content === 'يوسف') {  //WESO#0001
+      msg.reply('**يوسف مات**');  //WESO#0001
     }
   });
 
@@ -29,8 +29,8 @@ client.on('message', msg => {  //WESO#0001
   
 
   client.on('message', msg => {  //WESO#0001
-    if (msg.content === 'احبك') {  //WESO#0001
-      msg.reply('**حبك برص يا سافل**');  //WESO#0001
+    if (msg.content === 'مين اللي صممك') {  //WESO#0001
+      msg.reply('**الاسطوره يوسف  **');  //WESO#0001
     }
   });
 
